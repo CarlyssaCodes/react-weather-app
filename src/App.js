fef3b5f2dc2c0f750f1456cd6ab5ac8fe86d20bc
Carlyssa <78633277+CarlyssaCodes@users.noreply.github.com>
2021-09-05 22:04:03 -0400

    added repository link

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <p>
+        The project is{" "}
+        <a
+          href="https://github.com/CarlyssaCodes/react-weather-app"
+          target="_blank"
+        >
+          open-sourced
+        </a>{" "}
+        coded by Carlyssa Pierre
+      </p>
     </div>
   );
 }
