6d2d813aab44e05a4fb00c8b24fe4992c60300ee
Carlyssa <78633277+CarlyssaCodes@users.noreply.github.com>
2021-09-05 23:15:47 -0400

    Continued with html and added search engine form

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,35 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather">
-      <h1>Philadelphia</h1>
-      <h2>68°F</h2>
-      <p>Sunday 10:14pm</p>
-      <p>Overcast Clouds</p>
-      <p>Humidity: 65%</p>
-      <p>Wind: 2km/h</p>
+    <div className="container">
+      <div className="row">
+        <div className="Weather">
+          <form>
+            <input type="search" />
+            <input type="submit" />
+          </form>
+          <div className="col-6">
+            <h1>Philadelphia</h1>
+            <h2>68°F</h2>
+            <ul>
+              <li>Sunday 10:14pm</li>
+              <li>Overcast Clouds</li>
+              <li>Feels Like: 87°F</li>
+              <li>Humidity: 65%</li>
+              <li>Wind: 2km/h</li>
+            </ul>
+          </div>
+
+          <div className="col">
+            <img
+              src="http://openweathermap.org/img/wn/04n@2x.png"
+              alt="cloudy"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
